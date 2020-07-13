392b25debe639996e83b6ff0d860c1cd3e4ec785
raj.dixit_HTML <raj.dixit@appinventiv.com>
2020-07-13 19:26:47 +0530

    Made an UI for add aticle

--- src/app/modules/features/article-management/pages/add-article-management/add-article-management.module.ts
+++ src/app/modules/features/article-management/pages/add-article-management/add-article-management.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddArticleManagementComponent } from './view/add-article-management.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomImageModule } from 'src/app/pipes/custom-image/custom-image.module';
+import { MediaUploadModule } from 'src/app/modules/shared/media-upload/media-upload.module';
+import { MatFormFieldModule, MatIconModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatInputModule } from '@angular/material';
+import { ValidationErrorPipeModule } from 'src/app/pipes/validation-error/validation-error-pipe.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { WhiteSpaceModule } from 'src/app/modules/shared/white-space/white-space.module';
 
 
 const routes: Routes = [
@@ -15,7 +21,19 @@ const routes: Routes = [
   declarations: [AddArticleManagementComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CustomImageModule,
+    MediaUploadModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCheckboxModule,
+    CustomImageModule,
+    ValidationErrorPipeModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    WhiteSpaceModule
   ]
 })
 export class AddArticleManagementModule { }
