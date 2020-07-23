8de0a06d659a8be74af2f8968b3b56ee6b17af4e
raj.dixit_HTML <raj.dixit@appinventiv.com>
2020-07-23 18:14:55 +0530

    udpate the theme, UI of add-expert

--- src/app/modules/features/expert/pages/add-expert/add-expert.module.ts
+++ src/app/modules/features/expert/pages/add-expert/add-expert.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpertComponent } from './view/add-expert.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomImageModule } from 'src/app/pipes/custom-image/custom-image.module';
+import { MediaUploadModule } from 'src/app/modules/shared/media-upload/media-upload.module';
+import { MatFormFieldModule, MatButtonModule, MatIconModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { ValidationErrorPipeModule } from 'src/app/pipes/validation-error/validation-error-pipe.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { WhiteSpaceModule } from 'src/app/modules/shared/white-space/white-space.module';
 
 const routes: Routes = [
   {
@@ -14,7 +20,21 @@ const routes: Routes = [
   declarations: [AddExpertComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CustomImageModule,
+    MediaUploadModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    CustomImageModule,
+    MatSelectModule,
+    ValidationErrorPipeModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    WhiteSpaceModule
   ]
 })
 export class AddExpertModule { }
