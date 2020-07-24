6ed8bb39ef4bf1091c1aaa930d0e68b6f4c01acb
Ritu Tyagi <ritu.tyagi@appinventiv.com>
2020-07-24 17:33:57 +0530

    expert video page ui in progress

--- src/app/modules/features/expert/pages/expert-details/pages/expert-article/expert-article.module.ts
+++ src/app/modules/features/expert/pages/expert-details/pages/expert-article/expert-article.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExpertArticleComponent } from './view/expert-article.component';
+import { TableModule } from 'src/app/modules/commonTable/table';
+import { CustomDatePipeModule } from 'src/app/pipes/custom-date/custom-date-pipe.module';
 
 
 
 @NgModule({
   declarations: [ExpertArticleComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TableModule,
+    CustomDatePipeModule,
   ],
   exports: [ExpertArticleComponent]
 
--- src/app/modules/features/expert/pages/expert-details/pages/expert-video/expert-video.module.ts
+++ src/app/modules/features/expert/pages/expert-details/pages/expert-video/expert-video.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExpertVideoComponent } from './view/expert-video.component';
+import { MatFormFieldModule, MatInputModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 
 
 @NgModule({
   declarations: [ExpertVideoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [ExpertVideoComponent]
 })
