a55864f2a613ba636c5319436ae57be5743501b3
Ritu Tyagi <ritu.tyagi@appinventiv.com>
2020-07-28 15:08:58 +0530

    slider ui

--- src/app/modules/shared/slider/view/slider.component.ts
+++ src/app/modules/shared/slider/view/slider.component.ts
@@ -8,12 +8,38 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class SliderComponent implements OnInit {
 
+  slideIndex: number = 1;
+
+  testArr: any[] = [
+    "assets/images/imagePlaceholder.png",
+    "assets/images/inspiration.jpg",
+    "assets/images/user.png",
+  ]
+
   constructor(
-              private $dialogRef: MatDialogRef<SliderComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any
+    private $dialogRef: MatDialogRef<SliderComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   ngOnInit() {
+  }
+
+
+  // showSlides(slideIndex);
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  showSlides(n) {
+    let i;
+    let slides: any = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[this.slideIndex - 1].style.display = "block";
   }
 
 }
