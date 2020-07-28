cff1d3bc02ec125d348940302efd52cefd884f90
Ritu Tyagi <ritu.tyagi@appinventiv.com>
2020-07-28 18:58:27 +0530

    menu button and slider ui on user basic detail page

--- src/app/modules/features/users/pages/user-detail/user-basic-detail/user-basic-detail.module.ts
+++ src/app/modules/features/users/pages/user-detail/user-basic-detail/user-basic-detail.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserBasicDetailComponent } from './user-basic-detail.component';
 import { CustomDatePipeModule } from 'src/app/pipes/custom-date/custom-date-pipe.module';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatIconModule } from '@angular/material';
 import { CustomImageModule } from 'src/app/pipes/custom-image/custom-image.module';
 import { ViewFullImageModule } from 'src/app/modules/shared/view-full-image/view-full-image.module';
 import { SliderModule } from 'src/app/modules/shared/slider/slider.module';
@@ -17,7 +17,9 @@ import { SliderModule } from 'src/app/modules/shared/slider/slider.module';
     MatButtonModule,
     CustomImageModule,
     ViewFullImageModule,
-    SliderModule
+    SliderModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   exports: [UserBasicDetailComponent]
 })
