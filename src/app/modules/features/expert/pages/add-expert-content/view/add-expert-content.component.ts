e7851eb518f572f050be6c7e638f14ff8581eee2
shama <shama.parveen@appinventiv.com>
2020-07-28 18:44:48 +0530

    update code

--- src/app/modules/features/expert/pages/add-expert-content/view/add-expert-content.component.ts
+++ src/app/modules/features/expert/pages/add-expert-content/view/add-expert-content.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CategoryManagementService } from 'src/app/modules/features/category-management/service/category-management.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VALIDATION_CRITERIA } from 'src/app/constant/validation-criteria';
+import { EXPERT_DETAILS } from 'src/app/constant/routes';
+import { UtilityService } from 'src/app/modules/shared/services/utility.service';
+import { ExpertService } from '../../../service/expert.service';
 
 @Component({
   selector: 'app-add-expert-content',
@@ -10,24 +15,31 @@ import { CategoryManagementService } from 'src/app/modules/features/category-man
 export class AddExpertContentComponent implements OnInit {
   expertContentForm: FormGroup;
   categoryData: any;
+  expertContentId: string;
   constructor(
     private $fb: FormBuilder,
     private $category: CategoryManagementService,
+    $activatedRoute: ActivatedRoute,
+    private $router: Router,
+    private $service: ExpertService,
+    private $utility: UtilityService,
   ) {
+    this.expertContentId = $activatedRoute.snapshot.parent.params.id;
     this.createForm();
   }
 
   ngOnInit() {
+    this.categoryList();
   }
 
   createForm() {
     this.expertContentForm = this.$fb.group({
-      expertId: [],
+      expertId: [this.expertContentId],
       categoryId: [],
       price: [],
       contentId: [],
       mediaType: [],
-      description: [],
+      description: ['', [Validators.required, Validators.maxLength(VALIDATION_CRITERIA.descriptionMaxLength)]],
       privacy: []
     });
   }
@@ -41,10 +53,51 @@ export class AddExpertContentComponent implements OnInit {
   categoryList() {
     const params = {
       page: 1,
-      limit: 100,
+      limit: 10,
     };
     this.$category.queryData(params).then(res => {
       this.categoryData = res.data['data'];
     });
+  }
+
+  async onSubmit() {
+    if (this.expertContentForm.invalid) {
+      return;
+    }
+    // if (this.imageFile) {
+    //   let data: any = await this.$fileUploadService.uploadFile(this.imageFile);
+    //   this.profilePicURL = data.Location;
+    // }
+    let body = {...this.expertContentForm.value };
+    console.log(body);
+    // if (this.details && this.details._id) {
+    //   delete body.type;
+    //   this.$service.edit(this.details._id, body).then(
+    //     data => {
+    //       this.expertContentForm.enable();
+    //       this.$utility.success(data.message);
+    //       this.$route.navigate([EXPERT.fullUrl]);
+    //     },
+    //     err => {
+    //       this.expertContentForm.enable();
+    //     }
+    //   );
+    //   return;
+    // }
+    this.$service.add(body).then(
+      data => {
+        this.expertContentForm.enable();
+        this.$utility.success(data.message);
+
+        this.$router.navigate([EXPERT_DETAILS.fullUrl]);
+
+      },
+      err => {
+        this.expertContentForm.enable();
+      });
+  }
+
+  onCancel() {
+    this.$router.navigate([EXPERT_DETAILS.fullUrl]);
   }
 }
