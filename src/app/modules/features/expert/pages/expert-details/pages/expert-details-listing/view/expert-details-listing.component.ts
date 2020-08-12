6beb7f1f52eb5ec09c2c5e50602deed095749793
shama <shama.parveen@appinventiv.com>
2020-08-12 22:41:21 +0530

    formate data expert

--- src/app/modules/features/expert/pages/expert-details/pages/expert-details-listing/view/expert-details-listing.component.ts
+++ src/app/modules/features/expert/pages/expert-details/pages/expert-details-listing/view/expert-details-listing.component.ts
@@ -5,7 +5,7 @@ import { ExpertDetailsService } from '../../../service/expert-details.service';
 import { Router } from '@angular/router';
 import { ConfirmBoxService } from 'src/app/modules/shared/confirm-box';
 import { UtilityService } from 'src/app/modules/shared/services/utility.service';
-export type ActionType = 'deleted' ;
+export type ActionType = 'deleted' | 'blocked' | 'active';
 
 @Component({
   selector: 'app-expert-details-listing',
@@ -47,7 +47,7 @@ export class ExpertDetailsListingComponent implements OnInit, OnChanges {
   onActionHandler(id: string, action: ActionType) {
     console.log(this.experDetails);
     const index = this.experDetails.findIndex(user => user._id === id);
-    this.$confirmBox.listAction('post',  (action == 'deleted' ? 'Delete' : 'Block'))
+    this.$confirmBox.listAction('post',  action == 'active' ? 'Active' :(action == 'deleted' ? 'Delete' : 'Block'))
       .subscribe((confirm) => {
         if (confirm) {
           this.$expert.updateStatus(id, action).then((res) => {
@@ -64,12 +64,26 @@ export class ExpertDetailsListingComponent implements OnInit, OnChanges {
         this.experDetails.splice(index, 1);
         this.experDetails.total = this.experDetails.total - 1;
         break;
-      default:
-        break;
+        case 'active':
+           this.handleStatus(action, index);
+           break;
+        case 'blocked':
+          this.handleStatus(action, index);
+          break;
+        default:
+          break;
     }
     this.setUpTableResource(this.experDetails);
   }
 
+  handleStatus(action: 'blocked' | 'active', index: number) {
+    this.experDetails = this.experDetails.map((user, i) => {
+      if (i === index) {
+        user.status = action;
+      }
+      return user;
+    });
+  }
 
   setUpTableResource(record) {
     const { pageIndex, pageSize } = this.eventData;
