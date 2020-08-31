db884641a5e66d189a280072874986c3c0bcc0c5
Ritu Tyagi <ritu.tyagi@appinventiv.com>
2020-08-31 11:37:40 +0530

    report problem modal

--- src/app/modules/shared/report-problem/report-problem.module.ts
+++ src/app/modules/shared/report-problem/report-problem.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportProblemComponent } from './view/report-problem.component';
+import { MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 
 
 @NgModule({
   declarations: [ReportProblemComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   entryComponents: [ReportProblemComponent]
 })
