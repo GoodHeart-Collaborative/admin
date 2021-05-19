343a8a2f16cec02202d073d242086863e59b892b
shamaParveen <shama.parveen@appinventiv.com>
2021-05-19 14:36:52 +0530

    all code updated

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,7 @@ export const environment = {
   url: 'http://womencomdevapi.appskeeper.com/v1/',
   S3_BUCKET: "goodheart-admin",
   tokenKey: 'desknow-admin-token',
+    mapsApiKey: 'AIzaSyAlInsg850WrPN3xeqgT_muEvPdMJKAxYM',
   config: {
     AWS_ACCESS_KEY: 'AKIA6DQMUBGGZSBCXSFA',
     AWS_SECRET_KEY: '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',
